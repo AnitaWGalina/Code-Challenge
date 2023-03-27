a93a27acd8c7e699bf40717c12e8f287a2c0e883
anitagalina <anitagalina21@gmail.com>
2023-03-27 12:26:59 +0300

    Completed

--- Net Salary Calculator.js
+++ Net Salary Calculator.js
@@ -1,57 +1,117 @@
-function computeNetSalary(basicsalary ,rateoftax, nssfdeductions){
+function calculateIndividualNetSalary(){
+    const kraRate=[0.10,0.25,0.30]
+    const nhifRates=[150,300,400,500,600,750,850,900,950,1000,1100,1200,1300,1400,1500,1600,1700]
+    const nssfRate = 6/100;
+    
 
-    let rateoftax;
+    let basicSalary=parseInt(document.getElementById("basicSalary").value);
+    let benefits=parseInt(document.getElementById("benefits").value);
+    
+ 
+    let grossSalary = basicSalary + benefits;
+    
+  
+    let nssf = 0;
+    let tierOneEmployee=(grossSalary)*nssfRate
+    let tierTwoEmployee=(grossSalary-6000)*nssfRate
+    
+    if (grossSalary<=6000){
+    nssf=tierOneEmployee;
+    }else if (grossSalary>6000 && grossSalary<=18000){
+    nssf=360+tierTwoEmployee;
+    }else{
+    nssf= 1080;
+    };
     
     
     
-    if (basicsalary<= 24000){
-        rateoftax = ('10% of basicsalary');
-        
     
+    
+    
+    
+ 
+    
+    let taxableIncome = grossSalary-nssf;
+    
+    let payeeBeforeRelief = 0;
+    
+    
+    if (taxableIncome<=24000){
+    payeeBeforeRelief= taxableIncome*kraRate[0];
+    }else if(taxableIncome>24000 && taxableIncome<=32333){
+    payeeBeforeRelief=(24000*kraRate[0])+((taxableIncome-24000)*kraRate[1])
+    }else{
+    payeeBeforeRelief=(24000*kraRate[0])+((32333-24000)*kraRate[1])+((taxableIncome-32333)*kraRate[2])
+    
+    };
+    
+    
+  
+    
+    let personalRelief= 2400;
+
+    if (personalRelief>payeeBeforeRelief){
+    
+    payee=0;
+    
+    }else {
+    payee=payeeBeforeRelief-personalRelief;
     }
     
-    else if (basicsalary >= 24001 && basicsalary <= 32333){
-        rateoftax = ('25% of basicsalary');
-        
+    
+    
+  
+    let nhif = 0;
+    if (grossSalary <= 5999) {
+    nhif = nhifRates[0];
+    } else if (grossSalary>5999 && grossSalary <= 7999) {
+    nhif = nhifRates[1];
+    } else if (grossSalary>7999 &&grossSalary <= 11999) {
+    nhif = nhifRates[2];
+    } else if (grossSalary>11999 &&grossSalary <=14999) {
+    nhif = nhifRates[3];
+    }else if (grossSalary>14999 &&grossSalary <= 19999) {
+    nhif = nhifRates[4];
+    }else if (grossSalary>19999 &&grossSalary <= 24999) {
+    nhif = nhifRates[5];
+    }else if (grossSalary>24999 &&grossSalary <= 29999) {
+    nhif = nhifRates[6];
+    }else if (grossSalary>29999 &&grossSalary <= 34999) {
+    nhif = nhifRates[7];
+    }else if (grossSalary>34999 &&grossSalary <= 39999) {
+    nhif = nhifRates[8];
+    }else if (grossSalary>39999 &&grossSalary <= 44999) {
+    nhif = nhifRates[9];
+    }else if (grossSalary>44999 &&grossSalary <= 49999) {
+    nhif = nhifRates[10];
+    }else if (grossSalary>49999 &&grossSalary <= 59999) {
+    nhif = nhifRates[11];
+    }else if (grossSalary>59999 &&grossSalary <= 69999) {
+    nhif = nhifRates[12];
+    }else if (grossSalary>69999 &&grossSalary <= 79999) {
+    nhif = nhifRates[13];
+    }else if (grossSalary>79999 &&grossSalary <= 89999) {
+    nhif = nhifRates[14];
+    }else if (grossSalary>89999 &&grossSalary <= 99999) {
+    nhif = nhifRates[15];
+    }else {
+    nhif = nhifRates[16];
     }
+
+    let deductions = payee + nhif + nssf;
+    let netSalary = grossSalary - deductions;
     
-    else {
-        rateoftax = ('30% of basicsalary');
-       
+    
+  
+    document.getElementById("taxableIncome").value = taxableIncome;
+    document.getElementById("payeTax").value = payee;
+    document.getElementById("nhif").value = nhif;
+    document.getElementById("nssf").value = nssf;
+    document.getElementById("netpay").value =netSalary;
+    document.getElementById("gross").value = grossSalary;
+    document.getElementById("deductions").value = deductions;
+    document.getElementById("personalRelief").value = personalRelief;
+    
+    
     }
-    
-    }
-    
-    
-    
-    
-    let nssfdeductions;
-    
-    if (basicsalary>0 && basicsalary <=6000){
-        nssfdeductions=360
-    }
-    else if (basicsalary >=6001 && basicsalary>= 18000){
-        nssfdeductions=540
-    }
-    else {
-        nssfdeductions=1080
-    }
-    
-    
-    
-    let totalDeductions;
-    totalDeductions=rateoftax+nhifdeductions
-    
-    let netSalary;
-    netSalary = gross_salary-totalDeductions
-    console.log(netSalary);
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
--- Speed Detector.js
+++ Speed Detector.js
@@ -1,3 +1,4 @@
+function calculate(speed){
 if (speed <= 70){
 
     console.log ("OK");
@@ -10,3 +11,4 @@ if (speed <= 70){
     else{
         console.log("Points:"+((speed-70)/5));
     }
+}  
